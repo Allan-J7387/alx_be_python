5461b3c3971d5fa9a96e04f63aafc9c0b7018132
Jayallan Langat <langatjayallan@gmail.com>
2025-06-01 12:50:13 +0300

    Create basic_operations.py

---
+++ introduction_to_python/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+product = number1 * number2
+addition = number1 + number2
+subtraction = number1 - number2
+print("The product is ", str(product), "The sum is ", str(addition), "The subtraction is ", str(subtraction))
